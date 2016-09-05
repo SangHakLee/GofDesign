3f049c19b44143c95c390a1b417652cd97bf968f
Your Name <leesanghak7298@gmail.com>
2016-09-06 01:13:40 +0900

    싱글톤 패턴

---
+++ src/singletone/SystemSpeaker.java
@@ -0,0 +1,35 @@
+package singletone;
+
+public class SystemSpeaker {
+	
+	static private SystemSpeaker instance;
+	
+	private int volume;
+	
+	private SystemSpeaker(){
+		volume = 5;
+	}
+	
+	public static SystemSpeaker getInstance() {
+		
+		if(instance == null){ // 없을 때만 만든다.
+			instance = new SystemSpeaker();
+			System.out.println("새로 생성");
+		}else{
+			System.out.println("이미 생성");
+		}
+		
+		return instance;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	
+	
+}
